5c6d72abd4e3a1d34724dd4737b52c53f067e816
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-02 08:28:35 +0300

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by Oleg Tsarenkoff on 2.08.21.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
